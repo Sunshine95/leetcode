e543cbdfb73988c683bf9e2203b522484e4708af
Dushyant Goel <dushyant95goel@gmail.com>
2021-01-13 16:49:00 +0530

    leetcode 538

---
+++ 538.ConvertBSTtoGreaterTree.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    int sum;
+
+    void rnltra(TreeNode *node)
+    {
+
+        if (node == NULL)
+        {
+            return;
+        }
+
+        rnltra(node->right);
+        sum += node->val;
+        node->val = sum;
+        rnltra(node->left);
+    }
+
+    TreeNode *convertBST(TreeNode *root)
+    {
+
+        sum = 0;
+        rnltra(root);
+
+        return root;
+    }
+};
