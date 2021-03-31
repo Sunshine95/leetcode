b389b0910eae8a2b0cfbe5ae88a6c62331d40831
Dushyant Goel <dushyant95goel@gmail.com>
2021-03-31 15:56:00 +0530

    leetcode 1739

---
+++ 1739.BuildingBoxes.cpp
@@ -0,0 +1,24 @@
+//1739. Building Boxes
+
+class Solution
+{
+public:
+    int minimumBoxes(int n)
+    {
+
+        int dia_sz = 0, last_dia = 0;
+        while (n - (dia_sz + 1) * (dia_sz + 2) / 2 > 0)
+        {
+            dia_sz++;
+            n -= dia_sz * (dia_sz + 1) / 2;
+        }
+
+        while (n > 0)
+        {
+            last_dia++;
+            n -= last_dia;
+        }
+
+        return dia_sz * (dia_sz + 1) / 2 + last_dia;
+    }
+};
